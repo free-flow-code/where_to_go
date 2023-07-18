5f8a02450c4f3d86bf47ead813d613920445d568
free-flow-code <wildmuskaria8@gmail.com>
2023-07-18 21:46:56 +0400

    Move data from template to view.

--- places/views.py
+++ places/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Place
 
-# Create your views here.
+
+def show_index_page(request):
+    places = Place.objects.all()
+    context = {
+        "geo_json": {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+    }
+
+    for place in places:
+        place_features = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    place.lon,
+                    place.lat,
+                ]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.pk,
+                "detailsUrl": "/static/places/roofs24.json"
+            }
+        }
+        context["geo_json"]["features"].append(place_features)
+    return render(request, 'index.html', context=context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
