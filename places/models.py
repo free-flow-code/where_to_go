061163118d41a7d2b933c3cf02f34a585c25bca2
free-flow-code <wildmuskaria8@gmail.com>
2023-07-18 17:00:11 +0400

    Add Place model.

--- places/models.py
+++ places/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    imgs = models.ImageField(
+        blank=True,
+        upload_to='images',
+        verbose_name='Фото'
+    )
+    description_short = models.TextField(
+        max_length=400,
+        blank=True,
+        verbose_name='Короткое описание'
+    )
+    description_long = models.TextField(
+        blank=True,
+        verbose_name='Полное описание'
+    )
+    lat = models.FloatField(
+        blank=False,
+        verbose_name='Широта'
+    )
+    lon = models.FloatField(
+        blank=False,
+        verbose_name='Долгота'
+    )
+
+    def __str__(self):
+        return self.title
