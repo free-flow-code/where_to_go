86b5bb317dc6b0d69bff0d0cccb723a8766aa095
free-flow-code <wildmuskaria8@gmail.com>
2023-07-19 21:04:58 +0400

    Create API Endpoint and upload JSON.

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Place, Image
 
 
-admin.site.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Image)
--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Place
 
 
@@ -29,3 +30,18 @@ def show_index_page(request):
         }
         context["geo_json"]["features"].append(place_features)
     return render(request, 'index.html', context=context)
+
+
+def place_view(place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    place_details = {
+        "title": place.title,
+        "imgs": [image.image.url for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lon
+        }
+    }
+    return JsonResponse(place_details, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
