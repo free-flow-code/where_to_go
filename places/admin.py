69cea89140608e0baa55300aad240e420d4bd8de
free-flow-code <wildmuskaria8@gmail.com>
2023-07-21 16:23:49 +0400

    Adding a photo in the admin panel has been moved to the places menu.

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
-
-
-admin.site.register(Place, PlaceAdmin)
-admin.site.register(Image)
+    inlines = [ImageInline]
