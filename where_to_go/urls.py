41a110fd19293100f61b13e4669a5a239ca19b80
free-flow-code <wildmuskaria8@gmail.com>
2023-07-21 18:10:38 +0400

    WYSIWYG-editor included.

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 from places import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_index_page),
-    path('places/<int:place_id>', views.show_place_json, name='place_json')
+    path('places/<int:place_id>', views.show_place_json, name='place_json'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
