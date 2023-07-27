b806f56efa566541911a9027bfdc46d7bb4611a3
free-flow-code <wildmuskaria8@gmail.com>
2023-07-27 23:09:21 +0400

    Create list of places with list comprehension.

--- places/views.py
+++ places/views.py
@@ -6,30 +6,28 @@ from .models import Place
 
 def show_index_page(request):
     places = Place.objects.all()
+
     context = {
         "geo_json": {
             "type": "FeatureCollection",
-            "features": [],
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        place.lon,
+                        place.lat,
+                    ]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": reverse('place_json', args=[place.pk])
+                }
+            } for place in places]
         }
     }
 
-    for place in places:
-        place_features = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    place.lon,
-                    place.lat,
-                ]
-            },
-            "properties": {
-                "title": place.title,
-                "placeId": place.pk,
-                "detailsUrl": reverse('place_json', args=[place.pk])
-            }
-        }
-        context["geo_json"]["features"].append(place_features)
     return render(request, 'index.html', context=context)
 
 
